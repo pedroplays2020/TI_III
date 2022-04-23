1b4cea299dbd425c58f3da066f0a72b32c4684d6
pedroplays <pedromiguelrodrigues61@gmail.com>
2022-04-23 01:39:04 +0100

    aula 22-04-2022

---
+++ Cinco/Cinco/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Viatura.partida();
+            Viatura ford = new Viatura("Ford","G40");
+
+            ford.Velocidade = 12;
+
+            ford.mostrador();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+//ford.mostrador();
+//Viatura fiat = new Viatura("Fiat", "Fiat 600");
+////desreferenciar o objeto da memória que ocupava
+//fiat.mostrador();
+
+//ford = null;
+
+
+//GC.Collect();
+//GC.WaitForPendingFinalizers();
+//fiat.mostrador();
+//
---
+++ Cinco/Cinco/Viatura.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinco
+{
+    public class Viatura
+    {
+        public static int total;
+        public const int MAXVEL = 120; 
+        //campos
+        private String _marca;
+        public String Marca {
+            get
+            {
+                return _marca;
+            }
+        }
+
+        private String _modelo;
+
+        public String Modelo
+        {
+            get
+            {
+                return _modelo;
+            }
+        }
+        protected int _velocidade;
+        public int Velocidade
+        {
+            get {
+                return _velocidade;
+            }
+            set {
+                if (value >= 0) _velocidade = value;
+            }
+        }
+
+
+        public static void partida()
+        {
+            Console.WriteLine("------------Partida-------------");
+        }
+        public void mostrador()
+        {
+            Console.WriteLine("---------------("+  _modelo +")---------------");
+            Console.WriteLine("Velocidade Máxima:" + Viatura.MAXVEL.ToString());
+            Console.WriteLine(String.Format("Marca:{0} Velocidade:{1}", _marca, _velocidade));
+            Console.WriteLine("Total: " + Viatura.total.ToString());
+            Console.WriteLine("-------------------------------------");
+        }
+
+        //overloading
+        public Viatura(string marca, string modelo)
+        {
+            Viatura.total++;
+            this._marca = marca;
+            this._modelo = modelo;
+        }
+        public Viatura(string marca, string modelo, int velocidade):this(marca,modelo)
+        {
+            this._velocidade = velocidade;
+        }
+        //override
+        public override string ToString()
+        {
+            return "Marca:" + _marca + "  Modelo:" + _modelo + "  Velocidade:" + _velocidade.ToString();
+        }
+
+        ~Viatura()
+        {
+            Viatura.total--;
+            Console.WriteLine("Destrutor da viatura " + this._modelo);
+        }
+    }
+}
